d086e3280e6797f04b8720308c53b29d749d5e4b
Jelly Teapot <jacquelinttan@gmail.com>
2021-07-31 20:20:44 +0100

    Make gradient fill whole screen

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 /* Get current hour.*/
 let dateNow = new Date();
 let hNow = dateNow.getHours();
+
+function dawn() {
+    document.body.style.backgroundImage = 'linear-gradient(to top, #fddb92, #d1fdff)';
+}
+
+dawn();
